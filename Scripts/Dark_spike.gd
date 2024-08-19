b0710a5454cc178067822fb22c983313a8a0edd3
Rupert9623 <fuge.rupert@lindisfarne.school.nz>
2024-08-19 12:00:01 +1200

    LevelChange
    Version control, for code and scripts for level changing. Important as is a main check point

--- Scripts/Dark_spike.gd
+++ Scripts/Dark_spike.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D):
 	if body.name == "player":
 		body.respawn()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const SPEED = 400.0
 const JUMP_VELOCITY = -600.0
 
-var start_position = Vector2(600,250)
+var start_position = Vector2(0,0)
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -45,3 +45,4 @@ func _physics_process(delta):
 
 func respawn():
 	position = start_position
+
