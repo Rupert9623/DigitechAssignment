e8ab6028a9785fa6f71d00db69c69f329116ab22
Rupert9623 <fuge.rupert@lindisfarne.school.nz>
2024-08-19 12:00:01 +1200

    LevelChange
    Version control, for code and scripts for level changing. Important as is a main check point

---
+++ Scripts/Button_NextLevel.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Prefabs/arena.tscn")
+	pass # Replace with function body.
--- Scripts/Dark_spike.gd
+++ Scripts/Dark_spike.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
-func _on_body_entered(body: Node2D) -> void:
-	print("Ouch")
-	body.respawn()
+
+
+func _on_body_entered(body: Node2D):
+	if body.name == "player":
+		body.respawn()
