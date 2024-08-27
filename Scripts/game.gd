f18da00ee17d6fe43e40852ef7f67d74e11e15d9
Rupert9623 <fuge.rupert@lindisfarne.school.nz>
2024-08-27 12:20:32 +1200

    Score
    Score is now working, really hard to get working but now it works fine

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-
+var score = 0
 
 @onready var ArrowPrefab = preload("res://Prefabs/arrow.tscn")
 
@@ -8,12 +8,24 @@ extends Node2D
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Prefabs/arena.tscn")
+func _ready():
+	_update_ui()
 
 
 
 func _on_enemy_timer_timeout():
 	var enemy = ArrowPrefab.instantiate()
-	enemy.position = Vector2(1100,300)
+	var randomY = randi_range (70,700)
+	enemy.position = Vector2(1680, randomY)
 	add_child(enemy)
 	
+func _update_ui():
+	$GameUi/ScoreLabel.text = "SCORE: "+ str(score)
+	
+
+
+func _on_score_timer_timeout():
+	print("Score +1")
+	score += 1
+	_update_ui()
 	
